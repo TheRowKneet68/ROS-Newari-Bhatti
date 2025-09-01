d60b6814d337de503849a9ce0e99c941d41e3cce
TheRowKneet68 <rronit371@gmail.com>
2025-09-01 19:58:16 +0545

    Merge remote changes

--- app/order-confirmation/[id]/page.tsx
+++ app/order-confirmation/[id]/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from "react";
-import { supabase } from "@/utils/supabaseClient";
+import { supabase } from "@/utils/supabaseClient.ts";
 import Header from "@/components/Header";
 import Link from "next/link";
 
