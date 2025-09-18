1179b9817c213f5552e4a267c4f48ac19f535a31
Ronit Baniya Gupta <136110886+TheRowKneet68@users.noreply.github.com>
2025-09-19 02:15:49 +0545

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,9 +9,6 @@ import OrderModal from './OrderModal';
 import { createClient } from '@supabase/supabase-js';
 import { useRef } from 'react';
 
-
-
-
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
